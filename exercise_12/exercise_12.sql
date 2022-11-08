ea98ce3849aa170362c5214023c1bfbbf4f639b7
Charlie Hengemihle <charliehengemihle@gmail.com>
2022-11-08 16:06:38 -0700

    couple more tests, getting into the tricky ones

--- exercise_12/exercise_12.sql
+++ exercise_12/exercise_12.sql
@@ -1,3 +1,13 @@
 -- the top 10 cities with the most total rental payment amount
 -- include the city name and sum
 -- use SUM to sum up the amounts
+
+select city.city, sum(payment.amount)
+from payment
+left join customer on payment.customer_id = customer.customer_id
+left join address on customer.address_id = address.address_id
+left join city on address.city_id = city.city_id
+group by city.city
+order by sum(payment.amount) desc
+limit 10
+
